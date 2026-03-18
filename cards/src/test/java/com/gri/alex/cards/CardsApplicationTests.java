f9c538994bbd95c836b7e2750e422da67aeec11d
Alex Gridasov <AlexGridasov@users.noreply.github.com>
2026-03-18 16:12:19 +0200

    cards - Read configurations from the class path location of Config Server

--- cards/src/test/java/com/gri/alex/cards/CardsApplicationTests.java
+++ cards/src/test/java/com/gri/alex/cards/CardsApplicationTests.java
@@ -3,7 +3,7 @@ package com.gri.alex.cards;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {"build.version=1", "spring.cloud.config.enabled=false"})
 class CardsApplicationTests {
 
 	@Test
