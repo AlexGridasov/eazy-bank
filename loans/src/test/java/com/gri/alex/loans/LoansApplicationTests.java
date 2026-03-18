3f4318a98f7373d2e2dc93e083e3155df959616e
Alex Gridasov <AlexGridasov@users.noreply.github.com>
2026-03-18 16:11:57 +0200

    loans - Read configurations from the class path location of Config Server

--- loans/src/test/java/com/gri/alex/loans/LoansApplicationTests.java
+++ loans/src/test/java/com/gri/alex/loans/LoansApplicationTests.java
@@ -3,7 +3,7 @@ package com.gri.alex.loans;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {"build.version=1", "spring.cloud.config.enabled=false"})
 class LoansApplicationTests {
 
 	@Test
