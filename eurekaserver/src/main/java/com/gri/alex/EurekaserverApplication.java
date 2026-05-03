3f4aa7379aa9202f83726bac02974e70f25d685d
Alex Gridasov <AlexGridasov@users.noreply.github.com>
2026-05-03 22:55:03 +0300

    Setup Service Discovery agent using Eureka server

---
+++ eurekaserver/src/main/java/com/gri/alex/EurekaserverApplication.java
@@ -0,0 +1,15 @@
+package com.gri.alex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaserverApplication {
+
+  static void main(String[] args) {
+    SpringApplication.run(EurekaserverApplication.class, args);
+  }
+
+}
